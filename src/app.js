6927cb7f120c924eeb1d158cb00de47bc851668e
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-06-10 11:42:56 +0100

    Amending HTML h1 and li and adding API, axios in Javascript

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "f91c99b3703247e78d935ba99f21c950";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York,usa&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
+    
