2847b02927e31faf9e397d53f6867ba42e85a938
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-06-11 18:29:20 +0100

    Added class to li element to capitalize first letter in description

--- src/app.js
+++ src/app.js
@@ -57,5 +57,3 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
