a5e7364c5fb7297cfffeb1c101226f81ba371de9
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-06-15 14:14:01 +0200

    Fixing error in Java, APIkey was incorrect, added axios call

--- src/app.js
+++ src/app.js
@@ -50,12 +50,10 @@ function displayForecast() {
 }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
-    let apiKey = "91c99b3703247e78d935ba99f21c950";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-    lat=${coordinates.lat}&lon=${coordinates.lon}&appid${apiKey}&
-    units=metric`;
-    axios.get(apiUrl).then(getForecast);
+  console.log(coordinates);
+  let apiKey = "f91c99b3703247e78d935ba99f21c950";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
