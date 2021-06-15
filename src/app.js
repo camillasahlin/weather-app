3c10e8f400dced60a7a6cb42a98f21edbdf64617
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-06-15 13:43:36 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -49,6 +49,14 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "91c99b3703247e78d935ba99f21c950";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+    lat=${coordinates.lat}&lon=${coordinates.lon}&appid${apiKey}&
+    units=metric`;
+    axios.get(apiUrl).then(getForecast);
+}
 
 function displayTemperature(response) {
     console.log(response.data);
@@ -68,6 +76,9 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
+
 }
 
 function search(city) {
@@ -84,5 +95,3 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-displayForecast();
