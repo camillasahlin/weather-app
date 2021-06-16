4d5123f7c27e51366cfcac827b8e0bc26790391b
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-06-16 11:42:20 +0200

    Added code to Jave in forecast elements

--- src/app.js
+++ src/app.js
@@ -21,30 +21,41 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day]; 
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat"];
-    days.forEach(function(day) {
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
         forecastHTML = 
         forecastHTML + 
     `
                 <div class="col-2">
-                    <div class="weather-forecaste-date">${day}</div>
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" 
+                    <div class="weather-forecaste-date">${formatDay(forecastDay.dt)}</div>
+                    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                     alt=""
                     width="42" 
                     /><br />
                     <div class="weather-forecast-temperature">
                     <span class="weather-forecast-temperature-max">
-                        18°</span>
+                        ${Math.round(forecastDay.temp.max)}°</span>
                     <span class="weather-forecast-temperature-min">
-                        12°</span>
+                        ${Math.round(forecastDay.temp.min)}°</span>
                     </div>
                 </div>
             `;
-    })
+            }
+    });
         forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
