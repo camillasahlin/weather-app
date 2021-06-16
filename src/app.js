d9a7ae08dfb3664666e043ae8c965ba9653c8a13
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-06-16 11:59:15 +0200

    Changes made in HTML

--- src/app.js
+++ src/app.js
@@ -61,14 +61,12 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "f91c99b3703247e78d935ba99f21c950";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
-    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
